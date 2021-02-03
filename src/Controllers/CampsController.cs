393b2a92434f42c46bc1c44fee5ab606d913254f
Jeremy Francisco <jeremy.francisco@cognex.com>
2021-02-03 17:07:49 -0500

    Add GetCampByMoniker endpoint

--- src/Controllers/CampsController.cs
+++ src/Controllers/CampsController.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using CoreCodeCamp.Data;
@@ -12,8 +11,8 @@ namespace CoreCodeCamp.Controllers
     [Route("api/[controller]")]
     public class CampsController : ControllerBase
     {
-        private readonly ICampRepository _repository;
         private readonly IMapper _mapper;
+        private readonly ICampRepository _repository;
 
         public CampsController(ICampRepository repository, IMapper mapper)
         {
@@ -31,6 +30,21 @@ namespace CoreCodeCamp.Controllers
                 return this._mapper.Map<CampModel[]>(result);
             }
             catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+        }
+
+        [HttpGet("{moniker}")]
+        public async Task<ActionResult<CampModel>> Get(string moniker)
+        {
+            try
+            {
+                Camp result = await this._repository.GetCampAsync(moniker);
+
+                return result != null ? this._mapper.Map<CampModel>(result) : NotFound();
+            }
+            catch (Exception)
             {
                 return StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
             }
