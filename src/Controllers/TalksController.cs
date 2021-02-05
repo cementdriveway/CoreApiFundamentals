9fbb1dd254bea72a15dfd25dbe34373def7bc197
Jeremy Francisco <jeremy.francisco@cognex.com>
2021-02-04 20:02:15 -0500

    Add TalksController with Get endpoint to get all talks for a specific camp

---
+++ src/Controllers/TalksController.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using AutoMapper;
+using CoreCodeCamp.Data;
+using CoreCodeCamp.Data.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/camps/{moniker}/[controller]"), ApiController]
+    public class TalksController : ControllerBase
+    {
+        private readonly ICampRepository _campRepository;
+        private readonly LinkGenerator _linkGenerator;
+        private readonly IMapper _mapper;
+
+        public TalksController(ICampRepository campRepository, IMapper mapper, LinkGenerator linkGenerator)
+        {
+            this._mapper = mapper;
+            this._linkGenerator = linkGenerator;
+            this._campRepository = campRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<TalkModel[]>> Get(string moniker, bool includeSpeakers = false)
+        {
+            try
+            {
+                Talk[] talks = await this._campRepository.GetTalksByMonikerAsync(moniker, includeSpeakers);
+
+                return this._mapper.Map<TalkModel[]>(talks);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+        }
+    }
+}
