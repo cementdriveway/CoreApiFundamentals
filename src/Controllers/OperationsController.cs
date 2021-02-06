b38849892935eae0adebf97a5f6c1ee01bc9937e
Jeremy Francisco <jeremy.francisco@cognex.com>
2021-02-06 09:42:45 -0500

    Add OperationsController.cs as an example of a functional API call

---
+++ src/Controllers/OperationsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]"), ApiController]
+    public class OperationsController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+
+        public OperationsController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpOptions("reloadconfig")]
+        public IActionResult ReloadConfig()
+        {
+            try
+            {
+                IConfigurationRoot root = (IConfigurationRoot) this.configuration;
+
+                root.Reload();
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
