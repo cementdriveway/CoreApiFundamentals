706f0815d65f439e79311d97a9a26507cd6780ad
Jeremy Francisco <jeremy.francisco@cognex.com>
2021-02-06 08:46:50 -0500

    Add DataValidation to Talk Model

--- src/Data/Models/TalkModel.cs
+++ src/Data/Models/TalkModel.cs
@@ -1,11 +1,20 @@
-﻿namespace CoreCodeCamp.Data.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoreCodeCamp.Data.Models
 {
     public class TalkModel
     {
+        public int TalkId { get; set; }
+
+        [Required, StringLength(100)]
         public string Title { get; set; }
+
+        [Required, StringLength(4000, MinimumLength = 20)]
         public string Abstract { get; set; }
+
+        [Range(100, 400)]
         public int Level { get; set; }
-        
+
         public SpeakerModel Speaker { get; set; }
     }
 }
